9e7ffcdf527c8379b2cd68fd2cc1eaa166990320
evedl <82066881+evedl@users.noreply.github.com>
2022-05-09 01:28:28 -0500

    schema base

---
+++ models/Producto.js
@@ -0,0 +1,27 @@
+//esquema
+const mongoose = require('mongoose');
+
+const ProductoSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    categoria: {
+        type: String,
+        required: true
+    },
+    ubicacion: {
+        type: String,
+        required: true
+    },
+    precio: {
+        type: Number,
+        required: true
+    },
+    fechaCreacion: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('Peoducto', ProductoSchema);
